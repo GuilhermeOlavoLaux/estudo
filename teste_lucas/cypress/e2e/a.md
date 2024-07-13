14ace13b35602372353f696e2e8e50c62c2dbf6e
Guilherme Laux <lauxguilherme@hotmail.com>
2024-07-13 12:26:38 -0300

    Adiciona testes unitarios

--- teste_lucas/cypress/e2e/a.md
+++ teste_lucas/cypress/e2e/a.md
@@ -1,15 +1,29 @@
-# Scenarios for User Registration
+#10 Scenario: Date Not Filled In
 
-## #2 Scenario: Successful User Registration with Multiple Companies
+    Given the administrator is on the main page of the Contato Seguro system
+    And they select the "+Novo Usuário" button
+    When they fill in the "Nome" field with a valid name
+    And they fill in the "E-mail" field with a valid email
+    And they fill in the "Telefone" field with an area code and number
+    And they leave the "Data" field empty
+    And they select the desired company in the "Empresas" field
+    And they click the modal to close the dropdown menu
+    And they select the "Salvar" button
+    Then the system should alert the mandatory "Data" field
+    And display a message requesting the empty field to be filled in
 
-**Given** the administrator is on the main page of the Contato Seguro system  
-**And** selects the "+Novo Usuário" button  
-**When** they fill in the "Nome" field with a valid name  
-**And** they fill in the "E-mail" field with a valid email  
-**And** they fill in the "Telefone" field with an area code and number  
-**And** they fill in the "Data" field with a valid date  
-**And** they select the desired company in the "Empresas" field  
-**And** they select another company in the "Empresas" field  
-**And** they click the modal to close the dropdown menu  
-**And** they select the "Salvar" button  
-**Then** it should be possible to view the registered user on the main page
+
+.
+#13 Scenario: Company Not Selected
+
+    Given the administrator is on the main page of the Contato Seguro system
+    And they select the "+Novo Usuário" button
+    When they fill in the "Nome" field with a valid name
+    And they fill in the "E-mail" field with a valid email
+    And they fill in the "Telefone" field with an area code and number
+    And they fill in the "Data" field with a valid date
+    And they leave the "Empresas" field empty
+    And they click the modal to close the dropdown menu
+    And they select the "Salvar" button
+    Then the system should alert the mandatory "Empresas" field
+    And display a message requesting the empty field to be filled in
--- teste_lucas/cypress/e2e/app.cy.js
+++ teste_lucas/cypress/e2e/app.cy.js
@@ -26,6 +26,9 @@ class AddNewUserForm {
   get companiesInput() {
     return cy.get(".multiselect-container");
   }
+  get emptyCompanyFiledMessage() {
+    return cy.get("#swal2-html-container");
+  }
 
   typeName(text) {
     if (!text) return;
@@ -53,112 +56,292 @@ class AddNewUserForm {
   }
 }
 
+//1
 describe("New user registration", () => {
   describe("Successful User Registration", () => {
     it("Given the administrator is on the main page of the Contato Seguro system", () => {
       cy.visit("/");
     });
-
     it("And selects the +Novo Usuário button", () => {
       const addNewUserForm = new AddNewUserForm();
-
       addNewUserForm.addButton.click();
     });
-
     it("They fill in the Nome field with a valid name", () => {
       const addNewUserForm = new AddNewUserForm();
       addNewUserForm.typeName("Lucas Engelmann");
     });
-
     it("They fill in the E-mail field with a valid email", () => {
       const addNewUserForm = new AddNewUserForm();
       addNewUserForm.typeEmail("lucas@email.com");
     });
-
     it("They fill in the Telefone field with an area code and number", () => {
       const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.typeCellphone("51985696919");
+      addNewUserForm.typeCellphone("519132694345");
     });
-
     it("They fill in the Data field with a valid date", () => {
       const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.typeDate("2000-01-01");
+      addNewUserForm.typeDate("1997-01-01");
     });
-
     it("They select the desired company in the Empresas field", () => {
       const addNewUserForm = new AddNewUserForm();
       addNewUserForm.companiesInput.click();
       addNewUserForm.companiesInput.contains("Empresa 1").should("exist");
       cy.contains(".option", "Empresa 1").click();
+      cy.contains(".chip", "Empresa 1");
     });
     it("They click the modal to close the dropdown menu", () => {
       const addNewUserForm = new AddNewUserForm();
       addNewUserForm.modal.click();
     });
-
     it("They select the Salvar button", () => {
       const addNewUserForm = new AddNewUserForm();
       addNewUserForm.saveButton.click();
     });
-
     it("It should be possible to view the registered user on the main page", () => {
       cy.contains("Lucas Engelmann").should("exist");
       cy.contains("lucas@email.com").should("exist");
     });
   });
+  // //2
+  // describe("Successful User Registration with Multiple Companies", () => {
+  //   it("Given the administrator is on the main page of the Contato Seguro system", () => {
+  //     cy.visit("/");
+  //   });
+  //   it("And selects the +Novo Usuário button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.addButton.click();
+  //   });
+  //   it("They fill in the Nome field with a valid name", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeName("Lucas Engelmann");
+  //   });
+  //   it("They fill in the E-mail field with a valid email", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeEmail("lucas@email.com");
+  //   });
+  //   it("They fill in the Telefone field with an area code and number", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeCellphone("51985696919");
+  //   });
+  //   it("They fill in the Data field with a valid date", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeDate("2000-01-01");
+  //   });
+  //   it("They select the desired company in the Empresas field", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.companiesInput.click();
+  //     addNewUserForm.companiesInput.contains("Empresa 1").should("exist");
+  //     addNewUserForm.companiesInput.contains("Empresa 2").should("exist");
+  //     cy.contains(".option", "Empresa 1").click();
+  //     cy.contains(".option", "Empresa 2").click();
+  //     cy.contains(".chip", "Empresa 1");
+  //     cy.contains(".chip", "Empresa 2");
+  //   });
+  //   it("They click the modal to close the dropdown menu", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.modal.click();
+  //   });
+  //   it("They select the Salvar button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.saveButton.click();
+  //   });
+  //   it("It should be possible to view the registered user on the main page", () => {
+  //     cy.contains("Lucas Engelmann").should("exist");
+  //     cy.contains("lucas@email.com").should("exist");
+  //   });
+  // });
+  //3
+  // describe("Name Not Filled In", () => {
+  //   it("Given the administrator is on the main page of the Contato Seguro system", () => {
+  //     cy.visit("/");
+  //   });
+  //   it("And selects the +Novo Usuário button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.addButton.click();
+  //   });
+  //   it("When they leave the Nome field empty", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeName("");
+  //   });
+  //   it("They fill in the E-mail field with a valid email", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeEmail("lucas@email.com");
+  //   });
+  //   it("They fill in the Telefone field with an area code and number", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeCellphone("51985696919");
+  //   });
+  //   it("They fill in the Data field with a valid date", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeDate("2000-01-01");
+  //   });
+  //   it("They select the desired company in the Empresas field", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.companiesInput.click();
+  //     addNewUserForm.companiesInput.contains("Empresa 1").should("exist");
+  //     cy.contains(".option", "Empresa 1").click();
+  //     cy.contains(".chip", "Empresa 1");
+  //   });
+  //   it("They click the modal to close the dropdown menu", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.modal.click();
+  //   });
+  //   it("They select the Salvar button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.saveButton.click();
+  //   });
+  //   //TODO: Ajustar a maneira que isso esta sendo testado
+  //   it("Then the system should alert the mandatory Nome field and display a message requesting the empty field to be filled in", () => {
+  //     cy.get("input[required]").then(($input) => {
+  //       // A mensagem de validação está na propriedade 'validationMessage'
+  //       const validationMessage = $input[0].validationMessage;
+  //       expect(validationMessage).to.eq("Preencha este campo.");
+  //     });
+  //   });
+  // });
+  //4
+  // describe("Email Not Filled In", () => {
+  //   it("Given the administrator is on the main page of the Contato Seguro system", () => {
+  //     cy.visit("/");
+  //   });
+  //   it("And selects the +Novo Usuário button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.addButton.click();
+  //   });
+  //   it("When they fill in the Nome field with a valid name", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeName("Lucas Engelmann");
+  //   });
+  //   it("And they leave the E-mail field empty", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeEmail("");
+  //   });
+  //   it("They fill in the Telefone field with an area code and number", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeCellphone("51985696919");
+  //   });
+  //   it("They fill in the Data field with a valid date", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeDate("2000-01-01");
+  //   });
+  //   it("They select the desired company in the Empresas field", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.companiesInput.click();
+  //     addNewUserForm.companiesInput.contains("Empresa 1").should("exist");
+  //     cy.contains(".option", "Empresa 1").click();
+  //     cy.contains(".chip", "Empresa 1");
+  //   });
+  //   it("They click the modal to close the dropdown menu", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.modal.click();
+  //   });
+  //   it("They select the Salvar button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.saveButton.click();
+  //   });
+  //   //TODO: Ajustar a maneira que isso esta sendo testado
+  //   it("Then the system should alert the mandatory E-mail field and display a message requesting the empty field to be filled in", () => {
+  //     cy.get("input[required]").then(($input) => {
+  //       // A mensagem de validação está na propriedade 'validationMessage'
+  //       const validationMessage = $input[1].validationMessage;
+  //       expect(validationMessage).to.eq("Preencha este campo.");
+  //     });
+  //   });
+  // });
+  //5
+  // describe("Date Not Filled In", () => {
+  //   it("Given the administrator is on the main page of the Contato Seguro system", () => {
+  //     cy.visit("/");
+  //   });
+  //   it("And selects the +Novo Usuário button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.addButton.click();
+  //   });
+  //   it("When they fill in the Nome field with a valid name", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeName("Lucas Engelmann");
+  //   });
+  //   it("And they fill in the E-mail field with a valid email", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeEmail("lucas@email.com");
+  //   });
+  //   it("They fill in the Telefone field with an area code and number", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeCellphone("51985696919");
+  //   });
+  //   it("And they leave the Data field empty", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeDate("");
+  //   });
+  //   it("They select the desired company in the Empresas field", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.companiesInput.click();
+  //     addNewUserForm.companiesInput.contains("Empresa 1").should("exist");
+  //     cy.contains(".option", "Empresa 1").click();
+  //     cy.contains(".chip", "Empresa 1");
+  //   });
+  //   it("They click the modal to close the dropdown menu", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.modal.click();
+  //   });
+  //   it("They select the Salvar button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.saveButton.click();
+  //   });
+  //   //TODO: Ajustar a maneira que isso esta sendo testado
+  //   it("Then the system should alert the mandatory Data field and display a message requesting the empty field to be filled in", () => {
+  //     cy.get("input[required]").then(($input) => {
+  //       // A mensagem de validação está na propriedade 'validationMessage'
+  //       const validationMessage = $input[2].validationMessage;
+  //       expect(validationMessage).to.eq("Preencha este campo.");
+  //     });
+  //   });
+  // });
 
-  describe("Successful User Registration", () => {
-    it("Successful User Registration with Multiple Companies", () => {
-      cy.visit("/");
-    });
-
-    it("And selects the +Novo Usuário button", () => {
-      const addNewUserForm = new AddNewUserForm();
-
-      addNewUserForm.addButton.click();
-    });
-
-    it("They fill in the Nome field with a valid name", () => {
-      const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.typeName("Lucas Engelmann");
-    });
-
-    it("They fill in the E-mail field with a valid email", () => {
-      const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.typeEmail("lucas@email.com");
-    });
-
-    it("They fill in the Telefone field with an area code and number", () => {
-      const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.typeCellphone("51985696919");
-    });
-
-    it("They fill in the Data field with a valid date", () => {
-      const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.typeDate("2000-01-01");
-    });
-
-    it("They select the desired company in the Empresas field", () => {
-      const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.companiesInput.click();
-      addNewUserForm.companiesInput.contains("Empresa 1").should("exist");
-      addNewUserForm.companiesInput.contains("Empresa 2").should("exist");
-      cy.contains(".option", "Empresa 1").click();
-      cy.contains(".option", "Empresa 2").click();
-    });
-    it("They click the modal to close the dropdown menu", () => {
-      const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.modal.click();
-    });
-
-    it("They select the Salvar button", () => {
-      const addNewUserForm = new AddNewUserForm();
-      addNewUserForm.saveButton.click();
-    });
-
-    it("It should be possible to view the registered user on the main page", () => {
-      cy.contains("Lucas Engelmann").should("exist");
-      cy.contains("lucas@email.com").should("exist");
-    });
-  });
+  //6
+  // describe("Date Not Filled In", () => {
+  //   it("Given the administrator is on the main page of the Contato Seguro system", () => {
+  //     cy.visit("/");
+  //   });
+  //   it("And selects the +Novo Usuário button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.addButton.click();
+  //   });
+  //   it("When they fill in the Nome field with a valid name", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeName("Lucas Engelmann");
+  //   });
+  //   it("And they fill in the E-mail field with a valid email", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeEmail("lucas@email.com");
+  //   });
+  //   it("They fill in the Telefone field with an area code and number", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeCellphone("51985696919");
+  //   });
+  //   it("And they leave the Data field empty", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.typeDate("2000-01-02");
+  //   });
+  //   it("And they leave the Empresas field empty", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.companiesInput.click();
+  //     addNewUserForm.companiesInput.contains("Empresa 1").should("exist");
+  //   });
+  //   it("They click the modal to close the dropdown menu", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.modal.click();
+  //   });
+  //   it("They select the Salvar button", () => {
+  //     const addNewUserForm = new AddNewUserForm();
+  //     addNewUserForm.saveButton.click();
+  //   });
+  //   //TODO: Ajustar a maneira que isso esta sendo testado
+  //   it("Then the system should alert the mandatory Empresas field and display a message requesting the empty field to be filled in", () => {
+  //     addNewUserForm.emptyCompanyFiledMessage.should(
+  //       "contain",
+  //       "Insira as empresas do usuário!"
+  //     );
+  //   });
+  // });
 });
